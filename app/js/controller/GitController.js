90d4b4318a0633eef4c2b6372b90635c4491c015
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2018-11-30 13:39:10 -0200

    Add files via upload
    refatorando código - alterando layout - corrigindo bug do search de manter as tabelas

--- app/js/controller/GitController.js
+++ app/js/controller/GitController.js
@@ -5,15 +5,17 @@ class GitController{
         this.classHTMLRepos= document.querySelector(".tabela-repositorios");
         this.btn1= document.querySelector(".btn1");
         this.btn2= document.querySelector(".btn2");
-        this.renderizaUserBox=debounce(1000,this.renderizaUserBox.bind(this))
+        this.renderizaUserBox=debounce(1000,this.renderizaUserBox.bind(this));
         this.inputSearch = document.querySelector(".inputSearch");
         this.renderiza();
     };   
-    
-    renderiza(){        
+
+    //Une as chamadas de função/eventListeners para então ser chamado no constructor
+    renderiza(){            
         this.inputSearch.oninput= event =>{
         event.preventDefault();
         this.renderizaUserBox();
+        this.classHTMLRepos.innerHTML="";
         };
         
         this.btn1.onclick= event =>{
@@ -29,27 +31,40 @@ class GitController{
     
     };
 
+    //Renderiza as informações do usuário digitado
     renderizaUserBox(){
+
+        //Adiciona efeito de loading
         if(this.inputSearch.value){
             this.classHTMLFoto.innerHTML='<div class="lds-dual-ring"></div>';
             this.classHTMLTabela.innerHTML='<div class="lds-dual-ring"></div>';
         }else{
-            false
+            return false
         }
-        requestUserBox(this.inputSearch.value)    
-        .then(user =>{
-            this.classHTMLFoto.innerHTML= this.renderUserBoxFoto(user) ;
-            this.classHTMLTabela.innerHTML= this.renderUserBoxDados(user);
-        }).catch(() =>{
-            this.classHTMLFoto.innerHTML= "";
-            this.classHTMLTabela.innerHTML= "";
-                throw new Error("Erro na requisição");
-            });  
 
+        //Realiza o request
+        //Passa dados para função que monta a view
+        //Trata o erro caso o usuário não exista
+        requestUserBox(this.inputSearch.value)    
+            .then(user =>{
+                this.classHTMLFoto.innerHTML= this.renderUserBoxFoto(user) ;
+                this.classHTMLTabela.innerHTML= this.renderUserBoxDados(user);
+            }).catch(() =>{
+                this.classHTMLFoto.innerHTML= "";
+                this.classHTMLTabela.innerHTML= "";
+                    throw new Error("Erro na requisição");
+                });  
     };
 
+    //Renderiza tabela com repositórios
     renderizaRepo(){
+
+        //Adiciona efeito de loading
+        //Trata o erro caso o usuário  não exista
         this.load(this.classHTMLRepos, 
+
+            //Realiza requisição
+            //Passa dados para função que monta a view
             requestRepo(this.inputSearch.value)
             .then(dadosRepo =>{           
                 this.classHTMLRepos.innerHTML=this.renderRepoContent(dadosRepo);
@@ -57,8 +72,15 @@ class GitController{
         );
     };
 
+    //Renderiza tabela com favoritos
     renderizaFavoritos(){ 
+
+        //Adiciona efeito de loading
+        //Trata o erro caso o usuário  não exista
         this.load(this.classHTMLRepos,
+            //Realiza requisição
+            //Passa dados para função que monta  a view
+            //Trata o erro caso o usuário  não exista
             requestRepo(this.inputSearch.value)
             .then(res =>{            
                 this.classHTMLRepos.innerHTML= this.renderFavoritosContent(res);
@@ -67,6 +89,17 @@ class GitController{
         
     };
 
+    //Passa os dados para uma função que monta a view do box de dados do usuário
+    renderUserBoxDados(usuario){
+        return this.renderUserBoxDadosComportamento("Repositórios", usuario.public_repos)+
+               this.renderUserBoxDadosComportamento("Seguidores", usuario.following)+
+               this.renderUserBoxDadosComportamento("Seguindo", usuario.followers);
+                
+                
+    };
+
+    //Monta a view com a imagem do usuário
+    //Trata caso não consiga encontrar o usuário
     renderUserBoxFoto(usuario){
         return usuario.avatar_url? 
                     `<img alt="Imagem de usuário" src=${usuario.avatar_url}/>`
@@ -77,14 +110,8 @@ class GitController{
                     </div>`;
     };
 
-    renderUserBoxDados(usuario){
-        return this.renderUserBoxDadosComportamento("Repositórios", usuario.public_repos)+
-               this.renderUserBoxDadosComportamento("Seguidores", usuario.following)+
-               this.renderUserBoxDadosComportamento("Seguindo", usuario.followers);
-                
-                
-    };
-
+    //Monta a view com os dados do usuário
+    //Trata quando ele não existe
     renderUserBoxDadosComportamento(campo, dadoDoUsuario){
         if(dadoDoUsuario){
             return  `<h2>${campo} - ${dadoDoUsuario}</h2>`
@@ -97,18 +124,12 @@ class GitController{
         };
     };
 
-    renderRepo(repositorio){
-        return repositorio.map(res =>{return `
-            <tr>
-            <td>${res.name}</td><td>${dateHelper(res.updated_at)}</td>
-            </tr>
-            `}).join("");
-
-    };
-
+    //Monta a view da primeira parte da tabela de repositórios
+    //Chama uma função que monta a segunda parte
+    //Trata quando o usuário não possui repositórios
     renderRepoContent(repositorio){
         if(this.renderRepo(repositorio)){
-        return `      <thead>
+        return ` <thead>
                     <tr>
                     <th>Repositório</th>
                     <th>Última atualização</th>
@@ -126,20 +147,26 @@ class GitController{
                     </div>`;
                 };
    };
+   
+    //Monta a view da segunda parte da tabela de repositórios 
+    //Utiliza uma função para montar a data
+    renderRepo(repositorio){
+        return repositorio.map(res =>{
+            return `
+            <tr>
+                <td>${res.name}</td><td>${dateHelper(res.updated_at)}</td>
+            </tr>
+            `}).join("");
 
-    renderFavoritos(repositorio){
-        return this.renderFavoritosFilter(repositorio)
-            
-            .map(res => {return `
-                <tr>
-                <td>${res.name}</td><td>${res.stargazers_count}</td>
-                </tr>
-                `}).join("");
     };
 
+
+    //Monta a view da primeira parte da tabela de favoritos
+    //Chama uma função que monta a segunda parte
+    //Trata quando o usuário não possui favoritos
     renderFavoritosContent(repositorio){
         if(this.renderFavoritos(repositorio)){
-            return `
+            return `<h2>${this.teste()}}</h2>
                     <thead>
                         <tr>
                         <th>Repositório</th>
@@ -160,6 +187,8 @@ class GitController{
         };
     };
 
+    //Filta as estrelas de cada repositório
+    //Monta na ordem do maior pro menor
     renderFavoritosFilter(repositorio){
         return repositorio.filter(res =>{
             if(res.stargazers_count>0){return res}
@@ -167,8 +196,23 @@ class GitController{
             else{
                 return false
         };
+        }).sort((a,b) => b.stargazers_count-a.stargazers_count);
 
-    }).sort((a,b) => b.stargazers_count-a.stargazers_count);
+    };
+
+    //Monta a view da segunda parte da tabela de favoritos
+    renderFavoritos(repositorio){
+        return this.renderFavoritosFilter(repositorio)
+            
+            .map(res => {return `
+                <tr>
+                <td>${res.name}</td><td>${res.stargazers_count}</td>
+                </tr>
+                `}).join("");
+    };
+
+    teste(){
+        return requestUserBox(this.inputSearch.value).then(username => { return `${username.login}`})
     };
 
     load(classInnerHTML, fn){
@@ -181,7 +225,7 @@ class GitController{
                 classInnerHTML.innerHTML= "";
                 throw new Error("Erro na requisição")
             });           
-    }
+    };
 
 };
 
--- app/js/util/debounce.js
+++ app/js/util/debounce.js
@@ -1,3 +1,4 @@
+//Faz determinada função ser executada apenas depois do tempo desejado, apenas 1 vez
 function debounce(delay, fn) {
     let timer = 0;
     return () => {
